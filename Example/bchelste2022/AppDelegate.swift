4dbe4fa57827c26f33d5f45ed172190169ce07b8
bchelste <bchelste@gmail.com>
2022-08-22 03:41:40 +0300

    all ex done

--- Example/bchelste2022/AppDelegate.swift
+++ Example/bchelste2022/AppDelegate.swift
@@ -14,7 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        
+        
         // Override point for customization after application launch.
         return true
     }
--- Example/bchelste2022/ViewController.swift
+++ Example/bchelste2022/ViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import bchelste2022
 
 class ViewController: UIViewController {
-
+    
+    let articleManager = ArticleManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        podTest()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func podTest() {
+        print("--POD Test--")
+        
+        print("-- create #1 article")
+        let firstArticle = articleManager.newArticle()
+        firstArticle.creationDate = Date()
+        firstArticle.title = "firstArticle"
+        firstArticle.content = "Hello, World!"
+        firstArticle.language = "en"
+        print(firstArticle)
+        
+        print("-- create #2 article")
+        let secondArticle = articleManager.newArticle()
+        secondArticle.creationDate = Date()
+        secondArticle.title = "secondArticle"
+        secondArticle.content = "piscineIOS day 08"
+        secondArticle.language = "ru"
+        print(secondArticle)
+        
+        print("-- all Articles: ")
+        let allAriclesTest = articleManager.getAllArticles()
+        print(allAriclesTest)
+        
+        print("-- get Articles with language:")
+        let articlesLanguage = articleManager.getArticles(withLang: "ru")
+        print(articlesLanguage)
+        
+        print("-- get Articles contain string:")
+        let articlesContainString = articleManager.getArticles(containingString: "Hello")
+        print(articlesContainString)
+        
+        print("-- remove: (second Article)")
+        articleManager.removeArticle(article: secondArticle)
+        print("all articles after removing ->")
+        print(articleManager.getAllArticles())
+        
+        print("-- save remaining articles --")
+        articleManager.save()
+        
     }
 
 }
